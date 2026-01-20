da40fbd052635b4a882831625d1518d351e9fb19
raydon <auk7495@gmail.com>
2026-01-20 19:47:31 +0000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   "discordId": "1357703936576065566",
-  "apiUrl": "https://camilo404.azurewebsites.net/v1/",
+  "apiUrl": "https://lanyard.fbi.lat/v1/",
   "webSocketUrl": "wss://api.lanyard.rest/socket"
 };
